7637839023b04f340ff8d6668858fbe664d1c2a4
Redking <bakreridwan77@gmail.com>
2026-08-03 15:34:08 +0100

    Add files via upload

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,11 +1,14 @@
 import 'react-native-gesture-handler'; // MUST be the very first import, before anything else
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigator';
+import { AuthProvider } from './src/features/auth/authContext';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <RootNavigator />
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SafeAreaProvider>
+        <RootNavigator />
+      </SafeAreaProvider>
+    </AuthProvider>
   );
 }
