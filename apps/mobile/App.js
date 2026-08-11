b8d24feb2a1a7dcf57aa6b4d0bdeb0e112a3c921
Redking <bakreridwan77@gmail.com>
2026-08-11 16:35:08 +0100

    Add files via upload

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,26 +1,9 @@
 import 'react-native-gesture-handler'; // MUST be the very first import, before anything else
-import { useEffect } from 'react';
-import { Platform } from 'react-native';
-import * as NavigationBar from 'expo-navigation-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigator';
 import { AuthProvider } from './src/features/auth/authContext';
 
 export default function App() {
-
-  useEffect(() => {
-    // This hides the bottom digital buttons on Android
-    const setFullScreen = async () => {
-      if (Platform.OS === 'android') {
-        await NavigationBar.setVisibilityAsync('hidden');
-        // 'overlay-swipe' allows users to temporarily swipe up from the bottom to see the buttons if they need them
-        await NavigationBar.setBehaviorAsync('overlay-swipe');
-      }
-    };
-
-    setFullScreen();
-  }, []);
-
   return (
     <AuthProvider>
       <SafeAreaProvider>
